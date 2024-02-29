bbb3a1335673b70d511e8af8906a90ef6327da1f
B.M.R. Heijligers <bjorn@heijligers.com>
2024-02-29 21:06:40 +0100

    Update tags.scm to make it compatible with aider

--- queries/tags.scm
+++ queries/tags.scm
@@ -1,20 +1,20 @@
 (class_declaration
-  name: (type_identifier) @name) @definition.class
+  name: (type_identifier) @name.definition.class) @definition.class
 
 (protocol_declaration
-  name: (type_identifier) @name) @definition.interface
+  name: (type_identifier) @name.definition.interface) @definition.interface
 
 (class_declaration
     (class_body
         [
             (function_declaration
-                name: (simple_identifier) @name
+                name: (simple_identifier) @name.definition.class
             )
             (subscript_declaration
-                (parameter (simple_identifier) @name)
+                (parameter (simple_identifier) @name.definition.class)
             )
-            (function_declaration "init" @name)
-            (deinit_declaration "deinit" @name)
+            (function_declaration "init" @name.definition.class)
+            (deinit_declaration "deinit" @name.definition.class)
         ]
     )
 ) @definition.method
@@ -23,12 +23,12 @@
     (protocol_body
         [
             (protocol_function_declaration
-                name: (simple_identifier) @name
+                name: (simple_identifier) @name.definition.protocol
             )
             (subscript_declaration
-                (parameter (simple_identifier) @name)
+                (parameter (simple_identifier) @name.definition.protocol)
             )
-            (protocol_function_declaration "init" @name)
+            (protocol_function_declaration "init" @name.definition.protocol)
         ]
     )
 ) @definition.method
@@ -37,15 +37,15 @@
     (class_body
         [
             (property_declaration
-                (pattern (simple_identifier) @name)
+                (pattern (simple_identifier) @name.definition.class)
             )
         ]
     )
 ) @definition.property
 
 (property_declaration
-    (pattern (simple_identifier) @name)
+    (pattern (simple_identifier) @name.definition.property)
 ) @definition.property
 
 (function_declaration
-    name: (simple_identifier) @name) @definition.function
+    name: (simple_identifier) @name.definition.function) @definition.function
